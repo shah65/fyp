65f698aa1bb12d0e2b54313258170fa8c4b4b083
shah65 <ctech8868@gmail.com>
2026-01-04 11:00:17 +0500

    groupPageAdded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
- import awkumImage from './public/awkumimg1.png';
-import tchrlogo from './public/tchlogo.png';
-import stdlgo from './public/stdlogo.png';
 import Login from './components/pages/Login';
 import Logout from './components/pages/Logout';
 import Profile from './components/pages/Profile';
@@ -9,7 +6,8 @@ import Singup from './components/pages/Singup';
 import Home from './components/pages/Home';
 import AuthProvider from './components/context/AuthProvider';
 import ProtectedRoute from './components/routes/ProtectRoute';
-import Layout from './components/pages/Layout';
+ 
+ 
 
 const App = () => {
   
@@ -17,22 +15,28 @@ const App = () => {
     return (
       <AuthProvider>
         <Router>
-           
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route
-                path="/profile"
-                element={
-                  <ProtectedRoute>
-                    <Profile />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/logout" element={<Logout />} />
-              <Route path="/signup" element={<Singup />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-           
+          <Routes>
+            <Route path="/" element={<Home />} />
+            
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/logout"
+              element={
+                 
+                  <Logout />
+               
+              }
+            />
+            <Route path="/signup" element={<Singup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
         </Router>
       </AuthProvider>
     );
--- frontend/src/components/pages/Header.jsx
+++ frontend/src/components/pages/Header.jsx
@@ -8,7 +8,6 @@ const Header = ({user}) => {
     <header className="flex items-center w-full px-6">
       {/* Logo */}
       <div className="text-white hover:text-red-500  font-bold text-xl">AWKUM</div>
-       
 
       {/* Nav */}
       <nav className="flex space-x-4 ml-auto">
@@ -17,6 +16,12 @@ const Header = ({user}) => {
           className="text-white px-4 py-2 rounded-md hover:text-zinc-900 hover:bg-white/30 transition"
         >
           Home
+        </Link>
+        <Link
+          to="/group"
+          className="text-white px-4 py-2 rounded-md hover:text-zinc-900 hover:bg-white/30 transition"
+        >
+          Group
         </Link>
 
         <Link
